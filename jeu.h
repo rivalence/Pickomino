c43781f07325c119abeb1226e90af0e53125be60
Valence <49722266+rivalence@users.noreply.github.com>
2021-12-29 00:44:45 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -1,3 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
+
+typedef struct Element Element;
+typedef struct Liste Liste;
+typedef struct Joueur Joueur;
+typedef struct Pile Pile;
+struct Element
+{
+    int valeur;
+    int point;
+    Element *suivant;
+    Element *precedent;
+};
+
+struct Liste
+{
+    Element *premier;
+    Element *dernier;
+};
+
+struct Pile
+{
+    Element *dernier;
+};
+
+struct Joueur
+{
+    char nom[10];
+    Pile *pickominos;
+    int *table_joueur;
+    int score;
+};
+
+Liste *init();
+void affichepicko(Liste *);
+int *lancede(int);
